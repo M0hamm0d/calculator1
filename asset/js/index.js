c601f8c2ae3470f1648c3477a937dfd447aafadf
Muhammad Abdussalam <masalami042@gmail.com>
2024-07-10 07:59:23 +0100

    Implemented  another method to calculate apart from using eval function

--- asset/js/index.js
+++ asset/js/index.js
@@ -1,3 +1,58 @@
+/*
+ALGORITHM
+press one number and save it somewhere
+press operator and save it some where
+press second number and save it somewhere
+press equal sign to calculate
+*/
+let firstNumber;
+let secondNumber;
+let step = 0;
+let firstArr = [];
+let secondArr = []
+let operation;
+function getNumber(number) {
+    if(step == 0 || step ==1){
+        firstArr.push(number)
+        step = 1
+        firstNumber = Number(firstArr.join(''))
+        input.value = firstNumber
+    } else{
+        secondArr.push(Number(number))
+        secondNumber = Number(secondArr.join(''))
+        input.value = secondNumber
+    }
+}
+function getOperator(operator) {
+    step = 2
+    operation = operator
+};
+function clearDisplay() {
+    input.value = 0;
+    firstNumber = null;
+    secondNumber = null;
+    step = 0
+    firstArr = []
+    secondArr = []
+}
+function sum() {
+    if(operation === '+'){
+        let sum = firstNumber + secondNumber
+        input.value = sum
+    } else if(operation === '-'){
+        let sum = firstNumber - secondNumber
+        input.value = sum
+    } else if(operation === '/'){
+        let sum = firstNumber / secondNumber
+        input.value = sum
+    } else if(operation === '*'){
+        let sum = firstNumber * secondNumber
+        input.value = sum
+    }
+}
+
+
+
 const input = document.querySelector('#input');
 function displayOnScreen(value){
     input.value += value
@@ -8,3 +63,11 @@ function calculate(){
 function clearButton(){
     input.value = ''
 };
+console.log(input.value);
+function calc(operator, a, b) {
+    if (operator === 'add'){
+        return +a + +b
+    } else if (operator === 'sub'){
+        return +a - +b
+    }
+}
